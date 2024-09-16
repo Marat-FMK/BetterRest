a59f674cd403f9b1b12369ab669be870e4ac861b
MaratXcode <kosjachok8@yandex.ru>
2024-09-16 23:44:56 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marat Fakhrizhanov on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
